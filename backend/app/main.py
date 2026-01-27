066072cb8e6e5c549fec52092f1efb7a2c0a29f9
Nikhil Shanbhag <nikshanbhag@gmail.com>
2026-01-27 14:35:04 +0100

    Auto-commit: update Simplify Slovakia 2026-01-27 14:35:04

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,8 +17,8 @@ app = FastAPI(title="Simplify Slovakia API",
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
-    allow_credentials=True,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=False,  # Must be False when using "*"
     allow_methods=["*"],
     allow_headers=["*"],
 )
