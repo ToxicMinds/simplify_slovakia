79f022e5cfa7e6460fd7e6771ff0b4527d1beb06
Nikhil Shanbhag <nikshanbhag@gmail.com>
2026-02-08 20:10:04 +0100

    Auto-commit: Simplify Slovakia 2026-02-08 20:10:04

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -75,7 +75,7 @@ function App() {
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         {/* Show intake form */}
         {showIntake && (
-          <IntakeFlow onComplete={handleIntakeComplete} />
+          <IntakeForm onComplete={handleIntakeComplete} />
         )}
         
         {/* Show flow selector */}
